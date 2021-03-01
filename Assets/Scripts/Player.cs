13861aecb7d1d2426174039a4f495fd8e9688bfc
ij2049 <70619931+ij2049@users.noreply.github.com>
2021-03-02 03:57:43 +0900

    Week4_HW
    I make a shooting game.
    I added scores but it is still not working.
    I didn't have enough times to fix but I will fix it soon.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Hp;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Hp <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "bullet")
+        {
+            Hp -= 1;
+            Destroy(collision.gameObject);
+        }
+    }
+}
